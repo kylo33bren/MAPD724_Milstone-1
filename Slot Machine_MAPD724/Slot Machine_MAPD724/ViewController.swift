1c6dcc44e83bb0180ff423b95d25a9d9b043e0af
Brendan Rodrigues <49267534+kylo33bren@users.noreply.github.com>
2023-01-22 20:49:18 -0500

    Initial Commit

---
+++ Slot Machine_MAPD724/Slot Machine_MAPD724/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slot Machine_MAPD724
+//
+//  Created by Brendan Rodrigues on 2023-01-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
